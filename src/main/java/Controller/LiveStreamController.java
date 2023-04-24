434f5e3462c12edb881b9b566fa049ea5583010f
Elmira Madadi <elmiramadadi@gmail.com>
2023-04-24 23:04:57 +0200

    add LiveStreamController.java

--- src/main/java/Controller/LiveStreamController.java
+++ src/main/java/Controller/LiveStreamController.java
@@ -1,23 +1,4 @@
 package Controller;
 
-import model.LiveStream;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
-@RequestMapping("/streams")
 public class LiveStreamController {
-
-    private List<LiveStream> streams = new ArrayList<>();
-
-
-    //GET http://localhost: 8080/streams
-    @GetMapping
-    public List<LiveStream> findAll(){
-        return null;
-    }
 }
