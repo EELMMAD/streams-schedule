8fb2107c5c0f4b61b1ca47f4d64f098d361e6798
Elmira Madadi <elmiramadadi@gmail.com>
2023-04-25 13:44:40 +0200

    add CRUD methods

--- src/main/java/Controller/LiveStreamController.java
+++ src/main/java/Controller/LiveStreamController.java
@@ -1,12 +1,15 @@
 package Controller;
 
+import exception.LiveStreamNotFoundException;
+import jakarta.validation.Valid;
 import model.LiveStream;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import repository.LiveStreamRepository;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/streams")
@@ -14,14 +17,41 @@ public class LiveStreamController {
 
     private final LiveStreamRepository repository;
 
+    @Autowired
     public LiveStreamController(LiveStreamRepository repository) {
         this.repository = repository;
     }
 
-
-    //GET http://localhost: 8080/streams
+    //GET http://localhost:8080/streams
     @GetMapping
-    public List<LiveStream> findAll(){
+    public List<LiveStream> findAll() {
         return repository.findAll();
     }
+
+    //GET http://localhost:8080/streams/1242-2342-234234-234234
+    @GetMapping("/{id}")
+    public LiveStream findById(@PathVariable String id) throws LiveStreamNotFoundException {
+        return repository.findById(id);
+    }
+
+    //POST http://localhost:8080/streams
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LiveStream create(@Valid @RequestBody LiveStream stream) {
+        return repository.create(stream);
+    }
+
+    //PUT http://localhost:8080/streams/1242-2342-234234-234
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody LiveStream stream, @PathVariable String id) {
+        repository.update(stream,id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        repository.delete(id);
+    }
+
 }
